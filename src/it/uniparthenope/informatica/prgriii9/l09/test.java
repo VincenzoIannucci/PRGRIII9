da513ab90a318570370c2e9154391b704f39c014
Vincenzo Iannucci <34918941+enzo2605@users.noreply.github.com>
2021-11-12 11:48:38 +0100

    Retry

---
+++ src/it/uniparthenope/informatica/prgriii9/l09/test.java
@@ -0,0 +1,7 @@
+package it.uniparthenope.informatica.prgriii9.l09;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("You gotta get up and try.");
+    }
+}
